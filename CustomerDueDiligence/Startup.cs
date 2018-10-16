140961fbb27a4c8c89250af8628db7081da6da03
Oghenevwede Odia <Oghenevwede.Odia@ng.ey.com>
2018-10-16 23:19:41 +0100

    Add project files.

---
+++ CustomerDueDiligence/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomerDueDiligence.Startup))]
+namespace CustomerDueDiligence
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
